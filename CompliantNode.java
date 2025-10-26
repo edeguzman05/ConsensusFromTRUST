e257c02895efa1f172070434f2a3c84191ca6f3a
edeguzman05 <157542244+edeguzman05@users.noreply.github.com>
2025-10-26 13:12:17 -0700

    Add properties and constructor initialization in CompliantNode
    Added properties to CompliantNode class and initialized them in the constructor.

--- CompliantNode.java
+++ CompliantNode.java
@@ -3,13 +3,21 @@ import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
-
+    private double p_graph;
+    private double p_malicious;
+    private double p_txDistribution;
+    private int numRounds; 
+    private boolean[] followees;
+    
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+        this.p_graph = p_graph;
+        this.p_malicious = p.malicious
+        this.p_txDistrubution = p_txDistribution;
+        this.numRounds = numRounds;
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        this.followees = followees;
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
